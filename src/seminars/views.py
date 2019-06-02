0a074439581d297cf172dd7b2cbaad09855c77f5
Matheus <matheusmf.terapias@gmail.com>
2019-06-02 13:35:53 -0300

    Bring seminars template to seminars

--- src/seminars/views.py
+++ src/seminars/views.py
@@ -3,7 +3,15 @@ from django.views.generic import View
 
 
 class Seminars(View):
-    template_name = 'core/seminars.html'
+    template_name = 'seminars/seminars.html'
+
+    def get(self, request, *args, **kwargs):
+        context = {}
+        return render(request, self.template_name, context)
+
+
+class EspicifiedSeminar(View):
+    template_name = 'seminars/seminars.html'
 
     def get(self, request, *args, **kwargs):
         context = {}
