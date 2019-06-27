9d089eeb054a934eb1eafa64b634db3ad9b58da4
Matheus <matheusmf.terapias@gmail.com>
2019-06-27 19:33:09 -0300

    Adding the rest of the seminars

--- src/seminars/urls.py
+++ src/seminars/urls.py
@@ -2,7 +2,9 @@ from django.urls import include, path, re_path
 
 from .views import (Seminars, BasicDNASeminar, AdvancedDNASeminar, YouAndCreatorSeminar,
                     IntuitiveAnatomySeminar, InnerCircleSeminar, DNA3Seminar,
-                    RainbowChildrenSeminar, DigDeeperSeminar, ManifestingSeminar)
+                    RainbowChildrenSeminar, DigDeeperSeminar, ManifestingSeminar,
+                    SignificantOtherSeminar, EarthSeminar, WorldRelationsSeminar,
+                    PlanesSeminar, DiseaseSeminar)
 
 urlpatterns = [
     re_path(r'^$', Seminars.as_view(), name='seminars'),
@@ -10,9 +12,14 @@ urlpatterns = [
     path('dna-avancado/', AdvancedDNASeminar.as_view(), name='advanced-dna'),
     path('digging/', DigDeeperSeminar.as_view(), name='dig-deeper'),
     path('manifestacao-e-abundancia/', ManifestingSeminar.as_view(), name='manifesting'),
+    path('voce-e-seu-parceiro/', SignificantOtherSeminar.as_view(), name='significant-other'),
     path('voce-e-o-criador/', YouAndCreatorSeminar.as_view(), name='you-and-creator'),
+    path('voce-e-o-terra/', EarthSeminar.as_view(), name='you-and-earth'),
     path('anatomia-intuitiva/', IntuitiveAnatomySeminar.as_view(), name='intuitive-anatomy'),
+    path('relacoes-mundiais/', WorldRelationsSeminar.as_view(), name='world-relations'),
     path('voce-e-seu-circulo-intimo/', InnerCircleSeminar.as_view(), name='inner-circle'),
     path('dna3/', DNA3Seminar.as_view(), name='dna3'),
+    path('planos-de-existencia/', PlanesSeminar.as_view(), name='planes'),
+    path('doencas-e-desordens/', DiseaseSeminar.as_view(), name='disease-and-disorder'),
     path('crianca-arco-iris/', RainbowChildrenSeminar.as_view(), name='rainbow-children'),
 ]
--- src/seminars/views.py
+++ src/seminars/views.py
@@ -29,17 +29,32 @@ class DigDeeperSeminar(BaseSeminar):
 class ManifestingSeminar(BaseSeminar):
     template_name = 'seminars/manifesting.html'
 
+class SignificantOtherSeminar(BaseSeminar):
+    template_name = 'seminars/significant-other.html'
+
 class YouAndCreatorSeminar(BaseSeminar):
     template_name = 'seminars/you-and-creator.html'
 
 class InnerCircleSeminar(BaseSeminar):
     template_name = 'seminars/inner-circle.html'
 
+class EarthSeminar(BaseSeminar):
+    template_name = 'seminars/you-and-earth.html'
+
+class WorldRelationsSeminar(BaseSeminar):
+    template_name = 'seminars/world-relations.html'
+
 class IntuitiveAnatomySeminar(BaseSeminar):
     template_name = 'seminars/intuitive-anatomy.html'
 
+class PlanesSeminar(BaseSeminar):
+    template_name = 'seminars/planes.html'
+
 class DNA3Seminar(BaseSeminar):
     template_name = 'seminars/dna3.html'
+
+class DiseaseSeminar(BaseSeminar):
+    template_name = 'seminars/disease-and-disorder.html'
 
 class RainbowChildrenSeminar(BaseSeminar):
     template_name = 'seminars/rainbow-children.html'
