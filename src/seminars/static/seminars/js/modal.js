9f22aa09d327ce4cd7bd50aecf8cfdad5f2edad0
Matheus <matheusmf.terapias@gmail.com>
2020-04-27 16:24:24 -0300

    Fixing modal and Model

--- src/seminars/static/seminars/js/modal.js
+++ src/seminars/static/seminars/js/modal.js
@@ -3,12 +3,16 @@ var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
+var btn2 = document.getElementById("myBtn2");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
+  modal.style.display = "block";
+}
+btn2.onclick = function() {
   modal.style.display = "block";
 }
 
